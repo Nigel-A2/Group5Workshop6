b1574884a7a8f8fa5d39e2ee76e799a93fe137af
NigelOOSD <nigel.awrey@edu.sait.ca>
2022-03-28 16:29:59 -0500

    Added the fxml files and initializers

--- src/main/java/com/group5/workshop6/group5workshop6/AddModifyBooking.java
+++ src/main/java/com/group5/workshop6/group5workshop6/AddModifyBooking.java
@@ -1,4 +1,20 @@
 package com.group5.workshop6.group5workshop6;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class AddModifyBooking {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
--- src/main/java/com/group5/workshop6/group5workshop6/Booking.java
+++ src/main/java/com/group5/workshop6/group5workshop6/Booking.java
@@ -1,4 +1,5 @@
 package com.group5.workshop6.group5workshop6;
 
+// this is for the database table
 public class Booking {
 }
