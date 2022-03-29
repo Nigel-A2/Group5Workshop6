f214ad6ad0f154ccc1ce387c1057d02bf1a8672b
Nate Penner <natepenner@gmail.com>
2022-03-29 16:25:51 -0600

    Reworking project structure
    added mysql maven dependency. Removed lib folder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.group5.workshop6.group5workshop6 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+//    requires mysql.connector.java;
 
     opens com.group5.workshop6.group5workshop6 to javafx.fxml;
     exports com.group5.workshop6.group5workshop6;
