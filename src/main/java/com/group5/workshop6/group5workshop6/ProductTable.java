e7bde130c74c906af83d140eef39b1466acb6ae4
NigelOOSD <nigel.awrey@edu.sait.ca>
2022-03-31 15:24:50 -0500

    updated product-table-view

--- src/main/java/com/group5/workshop6/group5workshop6/ProductTable.java
+++ src/main/java/com/group5/workshop6/group5workshop6/ProductTable.java
@@ -2,8 +2,12 @@ package com.group5.workshop6.group5workshop6;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 
@@ -19,7 +23,17 @@ public class ProductTable {
     private Button btnAddProduct;
 
     @FXML
-    private TableView<?> tvProducts;
+    private TableView<Product> tvProducts;
+
+    @FXML
+    private TableColumn<Product, String> colProdName;
+
+    @FXML
+    private TableColumn<Product, Integer> colProductId;
+
+    private ObservableList<Product> productData = FXCollections.observableArrayList();
+
+    private String mode = "edit";
 
     @FXML
     void onAddProductClicked(MouseEvent event) {
@@ -30,6 +44,8 @@ public class ProductTable {
     void initialize() {
         assert btnAddProduct != null : "fx:id=\"btnAddProduct\" was not injected: check your FXML file 'product-table-view.fxml'.";
         assert tvProducts != null : "fx:id=\"tvProducts\" was not injected: check your FXML file 'product-table-view.fxml'.";
+        assert colProdName != null : "fx:id=\"colProdName\" was not injected: check your FXML file 'product-table-view.fxml'.";
+        assert colProductId != null : "fx:id=\"colProductId\" was not injected: check your FXML file 'product-table-view.fxml'.";
     }
 
 }
