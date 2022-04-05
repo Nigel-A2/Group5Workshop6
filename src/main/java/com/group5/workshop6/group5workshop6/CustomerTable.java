86be98ada7ec73a897c04a6ee70ad650c7a5df5a
NigelOOSD <nigel.awrey@edu.sait.ca>
2022-04-04 21:36:40 -0500

    I don't want to deal with merge conflicts

--- src/main/java/com/group5/workshop6/group5workshop6/CustomerTable.java
+++ src/main/java/com/group5/workshop6/group5workshop6/CustomerTable.java
@@ -1,19 +1,91 @@
 package com.group5.workshop6.group5workshop6;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 
 public class CustomerTable {
 
     @FXML
     private ResourceBundle resources;
-
     @FXML
     private URL location;
 
     @FXML
+    private Button btnAddCustomer;
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private TableView<Customer> tvCustomers;
+
+    @FXML
+    private TableColumn<Customer, Integer> colAgentId;
+
+    @FXML
+    private TableColumn<Customer, String> colCustAddress;
+
+    @FXML
+    private TableColumn<Customer, String> colCustBusPhone;
+
+    @FXML
+    private TableColumn<Customer, String> colCustCity;
+
+    @FXML
+    private TableColumn<Customer, String> colCustCountry;
+
+    @FXML
+    private TableColumn<Customer, String> colCustEmail;
+
+    @FXML
+    private TableColumn<Customer, String> colCustFirstName;
+
+    @FXML
+    private TableColumn<Customer, String> colCustHomePhone;
+
+    @FXML
+    private TableColumn<Customer, String> colCustLastName;
+
+    @FXML
+    private TableColumn<Customer, String> colCustPostal;
+
+    @FXML
+    private TableColumn<Customer, String> colCustProv;
+
+    @FXML
+    private TableColumn<Customer, Integer> colCustomerId;
+
+    @FXML
+    void onAddCustomerClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onHomeClicked(MouseEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert btnAddCustomer != null : "fx:id=\"btnAddCustomer\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert btnHome != null : "fx:id=\"btnHome\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert tvCustomers != null : "fx:id=\"tvCustomers\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colAgentId != null : "fx:id=\"colAgentId\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustAddress != null : "fx:id=\"colCustAddress\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustBusPhone != null : "fx:id=\"colCustBusPhone\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustCity != null : "fx:id=\"colCustCity\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustCountry != null : "fx:id=\"colCustCountry\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustEmail != null : "fx:id=\"colCustEmail\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustFirstName != null : "fx:id=\"colCustFirstName\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustHomePhone != null : "fx:id=\"colCustHomePhone\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustLastName != null : "fx:id=\"colCustLastName\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustPostal != null : "fx:id=\"colCustPostal\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustProv != null : "fx:id=\"colCustProv\" was not injected: check your FXML file 'customer-table-view.fxml'.";
+        assert colCustomerId != null : "fx:id=\"colCustomerId\" was not injected: check your FXML file 'customer-table-view.fxml'.";
 
     }
 
